0fa74277892231004aa603ac25c13109cd232df6
Elisey Zanko <elisey.zanko@gmail.com>
2016-07-16 13:48:56 +0500

    Fixed erors in test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 PROJECT_NAME=${PROJECT_NAME:-'hail'}
 STORM_VERSIONS=${STORM_VERSIONS:-'0.9.6 0.10.0 0.10.1 1.0.0 1.0.1'}
 PYTHON_VERSIONS=${PYTHON_VERSIONS:-'2.7'}
 
-trap "docker-compose -p ${PROJECT_NAME} down -v" EXIT INT TERM
+COMPOSE_FILE="$(dirname ${0})/docker-compose.yml"
+COMPOSE_COMMAND="docker-compose -f ${COMPOSE_FILE} -p ${PROJECT_NAME}"
+
+trap "${COMPOSE_COMMAND} down -v" EXIT INT TERM
 
 for storm in ${STORM_VERSIONS}; do
     for python in ${PYTHON_VERSIONS}; do
-        STORM_VERSION=${storm} PYTHON_VERSION=${python} docker-compose -p ${PROJECT_NAME} up --abort-on-container-exit --build
+        export STORM_VERSION=${storm}
+        export PYTHON_VERSION=${python}
+        ${COMPOSE_COMMAND} up --abort-on-container-exit --build
     done
 done
