6ffab46d470cf9d6cdd324b28deda318bb5f859c
Elisey Zanko <elisey.zanko@gmail.com>
2016-07-16 13:26:24 +0500

    Enhanced building

--- tests/test.sh
+++ tests/test.sh
@@ -4,9 +4,12 @@ set -e
 
 PROJECT_NAME=${PROJECT_NAME:-'hail'}
 STORM_VERSIONS=${STORM_VERSIONS:-'0.9.6 0.10.0 0.10.1 1.0.0 1.0.1'}
+PYTHON_VERSIONS=${PYTHON_VERSIONS:-'2.7'}
 
-trap "docker-compose -p ${PROJECT_NAME} down" EXIT INT TERM
+trap "docker-compose -p ${PROJECT_NAME} down -v" EXIT INT TERM
 
 for storm in ${STORM_VERSIONS}; do
-    STORM_VERSION=${storm} docker-compose -p ${PROJECT_NAME} up --abort-on-container-exit --build
+    for python in ${PYTHON_VERSIONS}; do
+        STORM_VERSION=${storm} PYTHON_VERSION=${python} docker-compose -p ${PROJECT_NAME} up --abort-on-container-exit --build
+    done
 done
